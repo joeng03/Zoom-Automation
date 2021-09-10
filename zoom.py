6722750c3e4b5ba049e44b43ffc14fedc27f6624
joeng03 <51855281+joeng03@users.noreply.github.com>
2021-09-10 10:33:41 +0800

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -6,55 +6,8 @@ import os
 import keyboard
 
 Days={'Mon':1,'Tue':2,'Wed':3,'Thu':4,'Fri':5,'Sat':6}
-Links={
-'班会' :
-'https://meet.google.com/lookup/hdp5wisszo',
-
-'华文': 
-'https://zoom.us/j/9023624412?pwd=OWJ0Vzg0S3MxT0JMS3BSb1JBRjMzQT09',
-
-'国文': 
-'https://us04web.zoom.us/j/4040910191?pwd=eldNS2JTS3VJRE5YWWwyWFlwKyt4UT09',
-
-'翻译' :
-'https://us02web.zoom.us/j/4996908916?pwd=RjNCcG8wZTE4OEk5ak1xbDR6b05vdz09',
-
-'英文' : 
-'https://us04web.zoom.us/j/7893283784?pwd=T2JNSE5kRy81eXNiTkVKZVgxTlZOdz09',
-
-'生物' : 
-'https://us02web.zoom.us/j/6315330904?pwd=VXJPUFoxeTY1eDlPK09yeXRocXRJdz09',
-
-'化学' : 
-'https://zoom.us/j/93437785753?pwd=NFE0bmNabjRHckcxNGR1K2drZ29WZz09',
-
-'物理' : 
-'https://us02web.zoom.us/j/9448860199?pwd=MEQxOWdnVmtZS0RHVG5FVU9VUGdndz09',
-
-'力学':
-'https://us02web.zoom.us/j/3090185152?pwd=ODc4M2NqZmpBK290ZDBTQ0N2M1MwQT09',
-
-'微积分': 
-'https://us02web.zoom.us/j/5826591153?pwd=NTB1em1yNWNLOGJuRVo0OWZ5Zngwdz09',
-
-'解几': 
-'https://us02web.zoom.us/j/5826591153?pwd=NTB1em1yNWNLOGJuRVo0OWZ5Zngwdz09',
-
-'美术': 
-'https://zoom.us/j/9675511790?pwd=YkFYQUpVSzdFV2tQQnJjWGxmdVZJUT09',
-
-'音乐':
-'https://us04web.zoom.us/j/9629766341?pwd=S3dxVFBBNzYwU1RXRG1PelJmOXhBZz09',
-
-'通识教育':
-'https://us02web.zoom.us/j/8585858858?pwd=QUtFTGNSK283cnZjditWRkdqMmNtZz09',
-
-'电脑':
-'https://meet.google.com/ikj-qhgf-igc?authuser=4',
-
-'体育':
-'https://us04web.zoom.us/j/5524167302?pwd=OHdkNUFyYmNVY0J6VkhwVDlSdmhRUT09'
-}
+Links={}
+#Links contains all the subject's names and their corresponding zoom links
 pyautogui.FAILSAFE=False
 def signIn(meeting_id,password):
     os.startfile(r"C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Zoom\Zoom.lnk")
@@ -100,6 +53,8 @@ def signIn(meeting_id,password):
             print(e)
     pyautogui.moveTo(maxbtn)
     pyautogui.click()
+    
+     #Records attendance if needed
     Chat=pyautogui.locateCenterOnScreen("Chat.png")
     while(Chat==None):
         try:
@@ -109,7 +64,7 @@ def signIn(meeting_id,password):
     pyautogui.moveTo(Chat)
     pyautogui.click()
     time.sleep(0.2)
-    pyautogui.write("Ng Yin Joe 65 160059")
+    pyautogui.write("YOUR NAME")
     pyautogui.press('enter')
     return
 
